f6706a3aa028ed9db2fa3f9d65682261570f5f24
create <create@mycreationhaven.com>
2026-03-09 03:41:23 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -23,6 +23,7 @@ import WorkforcePanel from "../components/panels/WorkforcePanel";
 import WorldChat from "../components/dashboard/WorldChat";
 import NationalAdvisorModal from "../components/modals/NationalAdvisorModal";
 import AIDiplomacyEngine from "../components/dashboard/AIDiplomacyEngine";
+import WorldEventBroadcaster from "../components/dashboard/WorldEventBroadcaster";
 
 export default function Dashboard() {
   const [myNation, setMyNation] = useState(null);
