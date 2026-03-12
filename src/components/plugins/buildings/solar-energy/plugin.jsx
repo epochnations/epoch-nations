4f741289809fdaf6b26e95ac9e566dccf0b0bb28
create <create@mycreationhaven.com>
2026-03-12 11:52:09 +0000

    File changes

---
+++ src/components/plugins/buildings/solar-energy/plugin.jsx
@@ -0,0 +1,10 @@
+{
+  "name": "Solar Energy Plant",
+  "version": "1.0.0",
+  "author": "EpochNations-Community",
+  "description": "Adds a Solar Energy Plant building that produces 50 energy per tick using renewable resources.",
+  "gameVersion": "0.1",
+  "type": "building",
+  "entry": "index.js",
+  "tags": ["energy", "green", "industrial"]
+}
