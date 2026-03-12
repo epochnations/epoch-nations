a4ea4242ee9d325563a781d5cf03d1f29cd9921e
create <create@mycreationhaven.com>
2026-03-12 11:53:26 +0000

    File changes

---
+++ src/components/plugins/languages/spanish/plugin.jsx
@@ -0,0 +1,10 @@
+{
+  "name": "Spanish Language Pack",
+  "version": "1.0.0",
+  "author": "EpochNations-Community",
+  "description": "Adds Spanish (Español) translations for all core game strings.",
+  "gameVersion": "0.1",
+  "type": "language",
+  "entry": "index.js",
+  "tags": ["language", "i18n", "español"]
+}
