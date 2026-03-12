c1e02061b884654826d8aefc3f7ad621b5c6e747
create <create@mycreationhaven.com>
2026-03-12 11:54:42 +0000

    File changes

---
+++ src/components/plugins/sdk/template/plugin.jsx
@@ -0,0 +1,10 @@
+{
+  "name": "My Plugin Name",
+  "version": "1.0.0",
+  "author": "YourName",
+  "description": "A short description of what this plugin does.",
+  "gameVersion": "0.1",
+  "type": "building",
+  "entry": "index.js",
+  "tags": []
+}
